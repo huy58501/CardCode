94956b882c02f8c26c0adead3fc91adcbe2d6414
huy5801 <huy5850@gmail.com>
2022-02-01 09:05:53 -0500

    updates new file

--- src/CardCode/CardCode.java
+++ src/CardCode/CardCode.java
@@ -1,20 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package ice1;
+package CardCode;
 
 /**
  *
  * @author chuong5850
  */
-public class ICE1 {
+public class CardCode {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         // TODO code application logic here
     }
-    
 }
