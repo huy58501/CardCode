7d86cffb9fe486e232af5f6082fc7519271d1216
huy5801 <huy5850@gmail.com>
2022-02-01 09:29:19 -0500

    updates card object

--- src/CardCode/CardStrick.java
+++ src/CardCode/CardStrick.java
@@ -9,11 +9,48 @@ package CardCode;
  * @author chuong5850
  */
 public class CardStrick{ 
-    /**
-     * @param args the command line arguments
-     */
+    private int cardValue;
+    private String cardSuit;
+
+    public CardStrick(int cardValue, String cardSuit) {
+        this.cardValue = cardValue;
+        this.cardSuit = cardSuit;
+    }
+
+    public CardStrick(int cardValue) {
+        this.cardValue = cardValue;
+    }
+    
+
+    public int getCardValue() {
+        return cardValue;
+    }
+
+    public void setCardValue(int cardValue) {
+        this.cardValue = cardValue;
+        if (cardValue == 1){
+            cardSuit = "Spades";
+        }
+        else if (cardValue == 2){
+            cardSuit = "Hearts";
+        }else if (cardValue == 3){
+            cardSuit = "Diamonds";
+        }else if (cardValue == 4){
+            cardSuit = "Clubs";
+        }
+        else System.out.println("The number should from 1 to 4");
+    }
+
+    public String getCardSuit() {
+        return cardSuit;
+    }
+
+    public void setCardSuit(String cardSuit) {
+        this.cardSuit = cardSuit;
+    }
+    
     public static void main(String args[]) {
         // TODO code application logic here
-        System.out.prinln("the card suit and value are: ");
+        CardStrick c = new CardStrick(2);
     }
 }
